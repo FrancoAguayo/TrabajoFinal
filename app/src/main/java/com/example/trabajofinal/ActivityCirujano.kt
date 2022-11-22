80cf2466bccd81fe75c1f21ba675edc85760fa82
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2022-11-22 10:53:28 -0300

    Push

--- app/src/main/java/com/example/trabajofinal/ActivityCirujano.kt
+++ app/src/main/java/com/example/trabajofinal/ActivityCirujano.kt
@@ -1,11 +1,39 @@
 package com.example.trabajofinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ActivityCirujano : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cirujano)
+        val botonResultados= findViewById<Button>(R.id.BtnNext5)
+        botonResultados.setOnClickListener(){
+            setData()
+        }
+    }
+    fun setData(){
+        val EtNombre= findViewById<EditText>(R.id.EtName3)
+        val EtApellido= findViewById<EditText>(R.id.EtSurname3)
+        val EtEdad= findViewById<EditText>(R.id.EtAge3)
+        val EtDNI= findViewById<EditText>(R.id.EtNDNI3)
+        val EtOperacion= findViewById<EditText>(R.id.EtSurgery)
+
+        val ConviertoEtNombre= EtNombre.text.toString()
+        val ConviertoEtApellido= EtApellido.text.toString()
+        val ConviertoEtEdad= EtEdad.text.toString()
+        val ConviertoEtDNI= EtDNI.text.toString()
+        val ConviertoEtOperacion= EtOperacion.text.toString()
+
+        val mostrarResultados3= Intent (this, ResultadosCirujano::class.java)
+        mostrarResultados3.putExtra("1",ConviertoEtNombre)
+        mostrarResultados3.putExtra("2",ConviertoEtApellido)
+        mostrarResultados3.putExtra("3",ConviertoEtEdad)
+        mostrarResultados3.putExtra("4",ConviertoEtDNI)
+        mostrarResultados3.putExtra("5",ConviertoEtOperacion)
+        startActivity(mostrarResultados3)
     }
 }
---
+++ app/src/main/java/com/example/trabajofinal/ResultadosCirujano.kt
@@ -0,0 +1,11 @@
+package com.example.trabajofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadosCirujano : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultados_cirujano)
+    }
+}
