9c7ef65f1e072455babc6627a7410916b409eabf
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2023-07-25 13:03:06 -0300

    Push

--- app/src/main/java/com/example/trabajofinal/ResultadosMedico.kt
+++ app/src/main/java/com/example/trabajofinal/ResultadosMedico.kt
@@ -4,8 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,24 +25,54 @@ class ResultadosMedico : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultados_medico)
-        val informe =findViewById<TextView>(R.id.TvInforme2)
-        val botonAvanzar= findViewById<Button>(R.id.BtnYes)
-        val botonRetroceder= findViewById<Button>(R.id.BtnNo)
-        val medicoRecibido=intent.getParcelableExtra<Medico>("medico")
-        val btnBucsar = findViewById<Button>(R.id.btnBuscar)
-        if(medicoRecibido!=null){
-            informe.text="El/La doctor(a) se llama ${medicoRecibido.nombre} ${medicoRecibido.apellido}." +
-                    " Tiene ${medicoRecibido.edad} años de edad y su DNI es ${medicoRecibido.dni};" +
-                    " su carrera como profesional en medicina tiene ${medicoRecibido.aniosExperiencia} años de experiencia" +
-                    " hasta la fecha con un titulo en ${medicoRecibido.tipoDoctorado}. Actualmente se está alistando para atender a todo" +
-                    " paciente que se le asigne."
-        }
-        botonAvanzar.setOnClickListener{
-            val avanzar= Intent (this, ResultadosFinales::class.java)
-            startActivity(avanzar)
-        }
-        botonRetroceder.setOnClickListener{
-            finish()
-        }
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create<ApiService>(ApiService::class.java)
+        getPostById()
     }
-}
+        fun getPostById() {
+            val informe =findViewById<TextView>(R.id.TvInforme2)
+            val botonAvanzar= findViewById<Button>(R.id.BtnYes)
+            val botonRetroceder= findViewById<Button>(R.id.BtnNo)
+            val medicoRecibido=intent.getParcelableExtra<Medico>("medico")
+            val btnBucsar = findViewById<Button>(R.id.btnBuscar)
+            val ivBuscar = findViewById<ImageView>(R.id.ivBuscar)
+            val etBuscar = findViewById<EditText>(R.id.etBuscar)
+            btnBucsar.setOnClickListener {
+                var posts: Post? = null
+                service.getPostById(etBuscar.text.toString().toInt()).enqueue(
+                    (object : Callback<Post> {
+                        override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                            posts = response.body()
+                            Picasso.get()
+                                .load(posts?.thumbnailUrl.toString())
+                                .into(ivBuscar)
+                            Log.i(TAG_LOGS, Gson().toJson(posts))
+                        }
+                        override fun onFailure(call: Call<Post>, t: Throwable) {
+                            t.printStackTrace()
+                        }
+                    })
+                )
+            }
+            if(medicoRecibido!=null){
+                informe.text="El/La doctor(a) se llama ${medicoRecibido.nombre} ${medicoRecibido.apellido}." +
+                        " Tiene ${medicoRecibido.edad} años de edad y su DNI es ${medicoRecibido.dni};" +
+                        " su carrera como profesional en medicina tiene ${medicoRecibido.aniosExperiencia} años de experiencia" +
+                        " hasta la fecha con un titulo en ${medicoRecibido.tipoDoctorado}. Actualmente se está alistando para atender a todo" +
+                        " paciente que se le asigne."
+            }
+            botonAvanzar.setOnClickListener{
+                val avanzar= Intent (this, ResultadosFinales::class.java)
+                startActivity(avanzar)
+            }
+            botonRetroceder.setOnClickListener{
+                finish()
+            }
+        }
+
+
+    }
